56160230d477296c82afd0c47023b50a7d9a7a3d
Taehoon Yoon <thyoon515@gmail.com>
2022-12-22 14:37:59 -0500

    Comment out seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create!(username: 'ThunderStorm', password: '1234', email: 'ThunderStorm@email.com')
+#User.create!(username: 'ThunderStorm', password: '1234', email: 'ThunderStorm@email.com')

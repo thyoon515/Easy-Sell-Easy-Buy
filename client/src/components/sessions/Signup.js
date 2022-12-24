f08569f22c3f61fd1dd92ae84992f5b013067dbe
thyoon515 <thyoon515@gmail.com>
2022-12-24 16:40:19 -0500

    Add styling to signup component

--- client/src/components/sessions/Signup.js
+++ client/src/components/sessions/Signup.js
@@ -1,5 +1,16 @@
 import { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 
 const Signup = () => {
@@ -9,6 +20,7 @@ const Signup = () => {
   const [email, setEmail] = useState('');
   const [errors, setErrors] = useState([]);
   
+  const theme = createTheme();
 
   const navigate = useNavigate();
 
@@ -36,34 +48,86 @@ const Signup = () => {
   }
 
   return (
-    <form onSubmit={handleSignupSubmit}>
-      <label>
-        Username:
-          <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
-      </label>
-      <br/>
-      <label>
-        Password:
-          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-      </label>
-      <br/>
-      <label>
-        Email:
-          <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} />
-      </label>
-      <br/>
-      <button type='submit'>Signup</button>
-      <br/>
-        <div>
-          {errors.length > 0 && (
-          <ul style={{ color: "red" }}>
-            {errors.map((error) => (
-              <li key={error}>{error}</li>
+    <ThemeProvider theme={theme}>
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <Box
+        sx={{
+          marginTop: 8,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign up
+        </Typography>
+        <Box component="form" noValidate onSubmit={handleSignupSubmit} sx={{ mt: 3 }}>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <TextField
+                required
+                fullWidth
+                id="username"
+                label="Username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                autoFocus
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                required
+                fullWidth
+                label="Password"
+                type="password"
+                id="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </Grid>
+          </Grid>
+          <div>
+           {errors && (
+           <ul style={{ color: "red" }}>
+             {errors.map((error) => (
+               <li key={error}>{error}</li>
             ))}
-          </ul>
-          )}
-        </div>
-    </form>
+           </ul>
+           )}
+          </div>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Sign Up
+          </Button>
+          <Grid container justifyContent="flex-end">
+            <Grid item>
+              <Link href="/login" variant="body2">
+                Already have an account? Sign in
+              </Link>
+            </Grid>
+          </Grid>
+        </Box>
+      </Box>
+    </Container>
+  </ThemeProvider>
   )
 }
 

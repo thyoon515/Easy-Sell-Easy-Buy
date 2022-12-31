0fcd1e2d082515007b62d11ca895003eadf09c89
Taehoon Yoon <thyoon515@gmail.com>
2022-12-31 14:26:05 -0500

    Add style to login component

--- client/src/components/sessions/Login.js
+++ client/src/components/sessions/Login.js
@@ -1,10 +1,26 @@
 import { useState } from 'react'
 import { useNavigate } from 'react-router-dom';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 const Login = ({ onLogin }) => {
 
+  const theme = createTheme();
+
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [errors, setErrors] = useState([]);
 
   const navigate = useNavigate();
 
@@ -17,10 +33,18 @@ const Login = ({ onLogin }) => {
       },
       body: JSON.stringify({ username }),
     })
-      .then((r) => r.json())
-      .then((user) => 
-      onLogin(user));
-      navigate('/');
+      .then(res => {
+        if(res.ok){
+            res.json().then((user) => {
+              onLogin(user)
+              navigate('/')
+            })
+        }else{
+            res.json().then((e) => {
+              setErrors(e.errors)
+            })
+        }
+      })
     }
   
   const handleUsernameChange = (e) => {
@@ -33,20 +57,78 @@ const Login = ({ onLogin }) => {
 
 
   return (
-    <form onSubmit={handleLoginSubmit}>
-        <label>
-          Username:
-            <input type="text" value={username} onChange={handleUsernameChange} />
-        </label>
-      <br />
-        <label>
-          Password:
-            <input type="password" value={password} onChange={handlePasswordChange
-            } />
-        </label>
-      <br />
-        <button type='submit'>Login</button>
-    </form>
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Login
+          </Typography>
+          <Box component="form" onSubmit={handleLoginSubmit} noValidate sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="username"
+              label="Username"
+              name="email"
+              autoFocus
+              value={username}
+              onChange={handleUsernameChange}
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              value={password}
+              onChange={handlePasswordChange}
+            />
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            />
+            <div>
+              {errors && (
+              <ul style={{ color: "red" }}>
+                {errors.map((error) => (
+                  <li key={error}>{error}</li>
+                ))}
+              </ul>
+              )}
+            </div>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Login
+            </Button>
+            <Grid container>
+              <Grid item>
+                <Link href="/signup" variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+      </Container>
+    </ThemeProvider>
   )
 }
 

2b3ecacf7aed3f5888814f30e4d0d7584b9531ac
Taehoon Yoon <thyoon515@gmail.com>
2023-01-31 14:47:03 -0500

    Remove purchase and sale attributes from Transaction Serializer

--- app/serializers/transaction_serializer.rb
+++ app/serializers/transaction_serializer.rb
@@ -1,5 +1,5 @@
 class TransactionSerializer < ActiveModel::Serializer
-  attributes :id, :post_date, :purchase, :sale, :buyer_id, :seller_id, :item_id
+  attributes :id, :post_date, :buyer_id, :seller_id, :item_id
 
   belongs_to :user
   belongs_to :item

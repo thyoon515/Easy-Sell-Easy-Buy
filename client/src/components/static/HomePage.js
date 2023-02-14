07a272d1db29686e6b1c4ab5178fb1db14801be3
Taehoon Yoon <thyoon515@gmail.com>
2023-02-14 14:13:38 -0500

    Add welcome log in image to Home component

--- client/src/components/static/HomePage.js
+++ client/src/components/static/HomePage.js
@@ -1,11 +1,29 @@
 import React from 'react'
+import gif from '/home/thyoon515/Development/code/phase-4/easy-sell-easy-buy/client/src/WelccomeToESEBgif.gif'
 
 const HomePage = ({ currentUser }) => {
 
   if (currentUser) {
-    return <h2>Welcome, {currentUser.username}!</h2>
+    return <div align='center' >
+              <h1>Welcome, {currentUser.username}!</h1>
+           </div>
   } else {
-    return <h2>Please Log in!</h2>
+    return <div> 
+              <img 
+                src={gif} 
+                alt='Welcome To ESEB' 
+                style={{
+                  position: 'absolute',
+                  top: 60,
+                  left: 0,
+                  right: 0,
+                  bottom: 0,
+                  height: '100%',
+                  width: '100%',
+                  objectFit: 'cover',
+                }}
+              />
+           </div>
   }
 }
 

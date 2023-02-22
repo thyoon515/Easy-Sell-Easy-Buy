1b2c10ad7ac8c2b2b0fa879f6e395bd34bc4fe59
Taehoon Yoon <thyoon515@gmail.com>
2023-02-22 16:45:35 -0500

    Reset database and add new seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,19 +7,19 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-# User.create(username: 'ThunderStorm', password: '1234', email: 'ThunderStorm@email.com')
-# User.create(username: 'Storm', password: '1234', email: 'Storm@email.com')
-# User.create(username: 'Tae', password: '1234', email: 'Tae@email.com')
+User.create(username: 'ThunderStorm', password: '1234', email: 'ThunderStorm@email.com')
+User.create(username: 'Storm', password: '1234', email: 'Storm@email.com')
+User.create(username: 'Tae', password: '1234', email: 'Tae@email.com')
 
-# Location.create(nyc_borough_name: 'The Bronx')
-# Location.create(nyc_borough_name: 'Queens')
-# Location.create(nyc_borough_name: 'Manhattan')
-# Location.create(nyc_borough_name: 'Brooklyn')
-# Location.create(nyc_borough_name: 'Staten Island')
+Location.create(nyc_borough_name: 'The Bronx')
+Location.create(nyc_borough_name: 'Queens')
+Location.create(nyc_borough_name: 'Manhattan')
+Location.create(nyc_borough_name: 'Brooklyn')
+Location.create(nyc_borough_name: 'Staten Island')
  
 
-# Item.create(title: "LG 65inch TV", image:"https://www.lg.com/us/images/tvs/md08000561/gallery/desktop-01.jpg", price: "$250", description: "Put your entertainment on full display.", user_id: User.first.id, location_id: Location.first.id)
-# Item.create(title: "Grand Theft Auto V - PlayStation 4", image:"https://media.gamestop.com/i/gamestop/10115462?fmt=auto&$pdp-gallery$", price: "$16.99", description: "When a young street hustler, a retired bank robber and a terrifying psychopath land themselves in trouble, they must pull off a series of dangerous heists to survive in a city in which they can trust nobody, least of all each other.", user_id: User.second.id, location_id: Location.second.id)
-# Item.create(title: "Office Chair", image:"https://m.media-amazon.com/images/I/71mKwaKglhL.jpg", price: "$64.99", description: "Brand new chair, very comfortable.", user_id: User.third.id, location_id: Location.third.id)
+Item.create(title: "LG 65inch TV", image:"https://www.lg.com/us/images/tvs/md08000561/gallery/desktop-01.jpg", price: "$250", description: "Put your entertainment on full display.", user_id: User.first.id, location_id: Location.first.id)
+Item.create(title: "Grand Theft Auto V - PlayStation 4", image:"https://media.gamestop.com/i/gamestop/10115462?fmt=auto&$pdp-gallery$", price: "$16.99", description: "When a young street hustler, a retired bank robber and a terrifying psychopath land themselves in trouble, they must pull off a series of dangerous heists to survive in a city in which they can trust nobody, least of all each other.", user_id: User.second.id, location_id: Location.second.id)
+Item.create(title: "Office Chair", image:"https://m.media-amazon.com/images/I/71mKwaKglhL.jpg", price: "$64.99", description: "Brand new chair, very comfortable.", user_id: User.third.id, location_id: Location.third.id)
 
 
